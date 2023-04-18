5f7741253e369d34ff8b68490a41d0b83c55fa5d
Jean Reynoso <jreynoso.mena@gmail.com>
2023-04-18 17:34:05 -0500

    feat: dockerfile were added

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import { MongoClient, Collection } from "mongodb";
 
-const url = "mongodb://localhost:27017";
+const url =
+  "mongodb://jnreynoso:shutdown-i123QWE@docdb-2023-04-18-22-24-20.cluster-cbhdrjtqlmgp.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 const dbName = "tokenizator";
 
 export class Database {
@@ -10,7 +11,11 @@ export class Database {
   constructor() {}
 
   async connect() {
-    this.client = new MongoClient(url, {});
+    this.client = new MongoClient(url, {
+      ssl: true,
+      sslValidate: true,
+      sslCA: "rds-combined-ca-bundle.pem",
+    });
     await this.client.connect();
     this.db = this.client.db(dbName);
   }
