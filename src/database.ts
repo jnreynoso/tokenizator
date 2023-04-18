756748e8591baab1a71b36fa3e1918653b66a131
Jean Reynoso <jreynoso.mena@gmail.com>
2023-04-18 18:09:40 -0500

    feat: move somethings

--- src/database.ts
+++ src/database.ts
@@ -1,26 +1,9 @@
-import dotenv from "dotenv";
-
 import { MongoClient, Collection } from "mongodb";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_USER = process.env.MONGO_USER;
-const MONGO_HOST = process.env.MONGO_HOST;
-const MONGO_PORT = process.env.MONGO_PORT;
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-
-console.log("**************************************************");
-console.log("**************************************************");
-console.log("**************************************************");
-console.log("**************************************************");
-console.log("**************************************************");
-console.log("**************************************************");
-console.log("**************************************************");
-console.log("**************************************************");
-console.log(process.env.MONGO_USER);
-
-const url = "mongodb://localhost:27017";
-//const url = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
+const url = process.env.MONGO_URI;
 const dbName = "tokenizator";
 
 export class Database {
